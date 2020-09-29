ebc22190bc337297db3d83bab83e69338745231a
JanHladik123 <60381843+JanHladik123@users.noreply.github.com>
2020-09-29 15:29:08 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,50 +1,43 @@
-<?php
-$auto = filter_input(INPUT_POST, 'auta');
-$spz = filter_input(INPUT_POST, 'spz');
-$submit = filter_input(INPUT_POST, 'submit');
-$types = array('Osobní', 'Nákladní', 'Autobus', 'Karavan', 'Trolejbus');
-$year = filter_input(INPUT_POST, 'year')
+<?php 
+
+define ('EUR_CZK', 27);
+$amount = filter_input(INPUT_POST, 'amount');
+$switch = filter_input(INPUT_POST, 'switch');
+$final = $amount * EUR_CZK;
+
+
+if($switch == 'czk_eur' )
+ ($final = $amount / 27);
+else ($final = $amount);
+ 
+
+$eur = filter_input(INPUT_POST, 'eur');
+$czk = 26.95;
+$sub = filter_input(INPUT_POST, 'odeslat');
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Převody</title>
 </head>
 <body>
+
 <?php
-if (isset($submit)) { ?>
-   Typ: <?= $auto ?><br>
-   SPZ: <?= $spz ?><br>
-   Rok: <?= $year ?> 
+if (isset($sub)) { ?>
+   Výsledek v Kč: <?= $eur * $czk ?>
 <?php
-if ($year >= 2010) { ?>
-    Moderní
-    <?php
-} elseif ($year >= 1991) { ?>
-    Ojeté
-    <?php
 } else { ?>
-    Veterán
-    <?php
-}
-
-}  else { ?>
-
     <form action="index.php" method="post">
-    Typ vozidla: <select name="auta" id="auta">
-        <?php foreach ($types as $type) { ?>
-                 <option value="<?= $type ?>"><?= $type ?></option>
-        <?php
-        } ?>    
-                </select>
-                <br>
-    SPZ:         <input type="text" name="spz"><br>
-    Rok výroby:  <input type="number" name="year" id="year" max="2020" min="1886"><br>
-                 <input type="submit" name="submit" value="Odeslat">
-</form>
+Převod:    <input type="text" name="eur" id="eur"> <br>
+        <input type="submit" value="odeslat" name="odeslat">
 
+
+    <input type="radio" name="switch" id="eur">EUR->CZK
+    <input type="radio" name="switch" id="czk">CZK->EUR
+    </form>
 <?php
 } ?>
 
